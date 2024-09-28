fee28be0323de69c6dc50d7f3ea9970eba566037
fxdamassa <afagundes777@gmail.com>
2024-09-28 09:55:48 -0300

    Adicionado rotas do google calendar

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,8 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::post('/register', [AuthController::class, 'register']);
 
+#Google Calendar Rotas
+Route::get('google/redirect', [GoogleController::class, 'redirectToGoogle'])->name('google.redirect');
+Route::get('google/callback', [GoogleController::class, 'handleGoogleCallback'])->name('google.callback');
+Route::post('calendar/create', [CalendarController::class, 'createEvent'])->name('calendar.create');
+Route::get('calendar/list', [CalendarController::class, 'listEvents'])->name('calendar.list');
