2e95104eb65996495fa7c16d873c4a4d80228e62
andreflik <andreflik@gmail.com>
2024-12-08 15:20:36 -0300

    feature(bootstrap/app.php): Realizado configuração no middleware para o HandleCors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,10 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
